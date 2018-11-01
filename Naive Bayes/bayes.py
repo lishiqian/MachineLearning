656b2c4583d7eb012a258304b7fca54026e4413a
lishiqiangit <2931164745@qq.com>
2018-11-01 21:43:56 +0800

    贝叶斯模块完成

--- Naive Bayes/bayes.py
+++ Naive Bayes/bayes.py
@@ -1,6 +1,6 @@
-#-*- coding: UTF-8 -*-
-import numpy as np
-from functools import reduce
+# -*- coding: UTF-8 -*-
+from numpy import *
+
 
 """
 函数说明:创建实验样本
@@ -24,5 +24,141 @@ def loadDataSet():
 				['stop', 'posting', 'stupid', 'worthless', 'garbage'],
 				['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
 				['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
-	classVec = [0,1,0,1,0,1]   																#类别标签向量，1代表侮辱性词汇，0代表不是
-	return postingList,classVec																#返回实验样本切分的词条和类别标签向量
+	classVec = [0,1,0,1,0,1]   			#类别标签向量，1代表侮辱性词汇，0代表不是
+	return postingList, classVec		#返回实验样本切分的词条和类别标签向量
+
+"""
+函数说明:将切分的实验样本词条整理成不重复的词条列表，也就是词汇表
+
+Parameters:
+	dataSet - 整理的样本数据集
+Returns:
+	vocabSet - 返回不重复的词条列表，也就是词汇表
+Author:
+	Jack Cui
+Blog:
+	http://blog.csdn.net/c406495762
+Modify:
+	2017-08-11
+"""
+def createVocabList(dataSet):
+	vocabSet = set([])
+	for document in dataSet:
+		vocabSet = vocabSet | set(document)
+	return list(vocabSet)
+
+"""
+函数说明:根据vocabList词汇表，将inputSet向量化，向量的每个元素为1或0
+
+Parameters:
+	vocabList - createVocabList返回的列表
+	inputSet - 切分的词条列表
+Returns:
+	returnVec - 文档向量,词集模型
+Author:
+	Jack Cui
+Blog:
+	http://blog.csdn.net/c406495762
+Modify:
+	2017-08-11
+"""
+def setOfWords2Vec(vocabList, inputSet):
+	returnVec = [0] * len(vocabList)
+	for word in inputSet:
+		if word in vocabList:
+			returnVec[vocabList.index(word)] = 1
+		else:
+			print "the word: %s is not in my vocabulaty" % word
+	return returnVec
+
+"""
+函数说明:朴素贝叶斯分类器训练函数
+
+Parameters:
+	trainMatrix - 训练文档矩阵，即setOfWords2Vec返回的returnVec构成的矩阵
+	trainCategory - 训练类别标签向量，即loadDataSet返回的classVec
+Returns:
+	p0Vect - 非的条件概率数组
+	p1Vect - 侮辱类的条件概率数组
+	pAbusive - 文档属于侮辱类的概率
+Author:
+	Jack Cui
+Blog:
+	http://blog.csdn.net/c406495762
+Modify:
+	2017-08-12
+"""
+def trainNB0(trainMatrix, trainCategory):
+	numTrainDocs = len(trainMatrix) #计算训练的文档数目
+	numWords = len(trainMatrix[0]) #计算每篇文档的词条数
+	pAbusive = sum(trainCategory) / float(numTrainDocs) #文档属于侮辱类的概率
+	p0Num = zeros(numWords); p1Num = zeros(numWords) #创建numpy.zeros数组
+	p0Denom = 0.0; p1Denom = 0.0 #分母初始化为0.0
+	for i in range(numTrainDocs):
+		if trainCategory[i] == 1:  #统计属于侮辱类的条件概率所需的数据，即P(w0|1),P(w1|1),P(w2|1)···
+			p1Num += trainMatrix[i]
+			p1Denom += sum(trainMatrix[i])
+		else:                       #统计属于非侮辱类的条件概率所需的数据，即P(w0|0),P(w1|0),P(w2|0）...
+			p0Num += trainMatrix[i]
+			p0Denom += sum(trainMatrix[i])
+	p1Vect = p1Num / p1Denom
+	p0Vect = p0Num / p0Denom
+	return p0Vect,p1Vect,pAbusive  #返回属于侮辱类的条件概率数组，属于非侮辱类的条件概率数组，文档属于侮辱类的概率
+
+
+"""
+函数说明:朴素贝叶斯分类器分类函数
+
+Parameters:
+	vec2Classify - 待分类的词条数组
+	p0Vec - 非侮辱类的条件概率数组
+	p1Vec -侮辱类的条件概率数组
+	pClass1 - 文档属于侮辱类的概率
+Returns:
+	0 - 属于非侮辱类
+	1 - 属于侮辱类
+Author:
+	Jack Cui
+Blog:
+	http://blog.csdn.net/c406495762
+Modify:
+	2017-08-12
+"""
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+	p1 = sum(vec2Classify * p1Vec) + log(pClass1)
+	p0 = sum(vec2Classify * p0Vec) + log(1.0 - pClass1)
+	if p1 > p0:
+		return 1
+	else:
+		return 0
+
+
+"""
+函数说明:测试朴素贝叶斯分类器
+
+Parameters:
+	无
+Returns:
+	无
+Author:
+	Jack Cui
+Blog:
+	http://blog.csdn.net/c406495762
+Modify:
+	2017-08-12
+"""
+def testingNB():
+	listOfPosts, listClasses = loadDataSet()
+	myVocabList = createVocabList(listOfPosts)
+	trainMat = []
+	for postinDoc in listOfPosts:
+		trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+	p0v, p1v, pAb = trainNB0(array(trainMat),array(listClasses))
+	testEntry = ['love', 'my', 'dalmation']
+	thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+	print testEntry,'classified as', classifyNB(thisDoc,p0v,p1v,pAb)
+	testEntry = ['stupid', 'garbage']
+	thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+	print testEntry, 'classified as', classifyNB(thisDoc, p0v, p1v, pAb)
+
+testingNB()
